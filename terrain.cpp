d1a2f5f9a386211ddbfc6ed432ae6bd56bc78ec8
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-05-29 17:31:11 +0100

    Enable terrain tiles to be converted to GeoTIFF
    This does not make use of the water mask: a future improvement should
    probably add the mask as an alpha band to the tiff.

--- terrain.cpp
+++ terrain.cpp
@@ -2,14 +2,43 @@
 #include <iostream>
 #include <vector>
 #include "src/TerrainTile.hpp"
+#include "src/GlobalGeodetic.hpp"
 
-/*void terrain2tiff(TerrainTile &terrain, double minx, double miny, double maxx, double maxy) {
+#include "gdal_priv.h"
+#include "ogr_spatialref.h"
+
+void terrain2tiff(TerrainTile &terrain, double minx, double miny, double maxx, double maxy) {
   double resolution = (maxx - minx) / 65;
-  }*/
+  double adfGeoTransform[6] = { minx, resolution, 0, maxy, 0, -resolution };
+
+  OGRSpatialReference oSRS;
+  oSRS.importFromEPSG(4326);
+
+  GDALDriverH hDriver = GDALGetDriverByName( "GTiff" );
+  GDALDatasetH hDstDS;
+  GDALRasterBandH hBand;
+
+  char *pszDstWKT = NULL;
+  oSRS.exportToWkt( &pszDstWKT );
+
+  hDstDS = GDALCreate(hDriver, "9-509-399.terrain.tif", 65, 65, 1, GDT_Int16, NULL );
+  GDALSetProjection( hDstDS, pszDstWKT );
+  CPLFree( pszDstWKT );
+  GDALSetGeoTransform( hDstDS, adfGeoTransform );
+
+  hBand = GDALGetRasterBand( hDstDS, 1 );
+  GDALRasterIO( hBand, GF_Write, 0, 0, 65, 65,
+                terrain.mHeights, 65, 65, GDT_Int16, 0, 0 );
+
+  // Once we're done, close properly the dataset
+  GDALClose( hDstDS );
+}
 
 int main() {
   TerrainTile terrain;
-  FILE *terrainIn = fopen("0.terrain", "rb");
+  FILE *terrainIn = fopen("9-509-399.terrain", "rb");
+
+  GDALAllRegister();
 
   try {
     terrain = TerrainTile(terrainIn);
@@ -44,7 +73,20 @@ int main() {
     printf("Has a NE child\n");
   }
 
-  std::vector<short int> heights = terrain.heights();
+  if (terrain.isMixed()) {
+    printf("The tile is a mixture of land and water\n");
+  } else if (terrain.isLand()) {
+    printf("The tile is land\n");
+  } else if (terrain.isWater()) {
+    printf("The tile is water\n");
+  } else {
+    // should not get here!!
+    printf("Unknown tile type!!\n");
+  }
+
+
+
+  /*std::vector<short int> heights = terrain.heights();
   std::vector<short int>::iterator heightsIterator;
   for(heightsIterator = heights.begin();
       heightsIterator != heights.end();
@@ -60,7 +102,16 @@ int main() {
       maskIterator++)
     {
       std::cout << "mask: " << *maskIterator << std::endl;
-    }
+      }*/
+
+  GlobalGeodetic profile;
+  double minLon, minLat, maxLon, maxLat;
+  short int zoom = 9;
+  int tx = 509, ty = 399;
+  profile.tileBounds(tx, ty, zoom, minLon, minLat, maxLon, maxLat);
+
+  //terrain2tiff(terrain, -180, -90, 0, 90);
+  terrain2tiff(terrain, minLon, minLat, maxLon, maxLat);
 
   FILE *terrainOut = fopen("out.terrain","wb");
   terrain.writeFile(terrainOut);
