2824f6a9fd8eaaa2ac7eeb8691865f36e4a55770
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-06-10 11:49:47 +0100

    Added the `--output-dir` option to `terrain-tile-bounds`

--- terrain-tile-bounds.cpp
+++ terrain-tile-bounds.cpp
@@ -9,13 +9,21 @@
 
 using namespace std;
 
+#ifdef _WIN32
+static const char *osDirSep = "\\";
+#else
+static const char *osDirSep = "/";
+#endif
+
 class TerrainTileBounds : public Command {
 public:
   TerrainTileBounds(const char *name, const char *version) :
-    Command(name, version)
+    Command(name, version),
+    outputDir(".")
   {}
 
-  void check() const {
+  void
+  check() const {
     switch(command->argc) {
     case 1:
       return;
@@ -30,29 +38,38 @@ public:
     help();                   // print help and exit
   }
 
-  const char * getInputFilename() const {
+  static void
+  setOutputDir(command_t *command) {
+    static_cast<TerrainTileBounds *>(Command::self(command))->outputDir = command->arg;
+  }
+
+  const char *
+  getInputFilename() const {
     return  (command->argc == 1) ? command->argv[0] : NULL;
   }
+
+  const char *outputDir;
 };
 
 static void printCoord(ofstream& stream, double x, double y) {
   stream << "[" << x << ", " << y << "]";
 }
 
-static void writeBounds(GDALTiler &tiler) {
+static void writeBounds(GDALTiler &tiler, const char *outputDir) {
   int tminx, tminy, tmaxx, tmaxy;
   ofstream geojson;
   short int maxZoom = tiler.maxZoomLevel();
   GlobalGeodetic profile = tiler.profile();
+  const string dirname = string(outputDir) + osDirSep;
 
   for (short int zoom = maxZoom; zoom >= 0; zoom--) {
     tiler.lowerLeftTile(zoom, tminx, tminy);
     tiler.upperRightTile(zoom, tmaxx, tmaxy);
 
-    string filename = static_cast<ostringstream*>( &(ostringstream() << zoom << ".geojson") )->str();
+    const string filename = dirname + static_cast<ostringstream*>( &(ostringstream() << zoom << ".geojson") )->str();
     cout << "creating " << filename << endl;
-    geojson.open(filename.c_str());
 
+    geojson.open(filename.c_str());
     geojson << "{ \"type\": \"FeatureCollection\", \"features\": [" << endl;
 
     for (int tx = tminx; tx <= tmaxx; tx++) {
@@ -88,6 +105,7 @@ static void writeBounds(GDALTiler &tiler) {
 int main(int argc, char *argv[]) {
   TerrainTileBounds command = TerrainTileBounds(argv[0], "0.0.1");
   command.setUsage("GDAL_DATASET");
+  command.option("-o", "--output-dir <dir>", "specify the output directory for the geojson files (defaults to working directory)", TerrainTileBounds::setOutputDir);
 
   // Parse and check the arguments
   command.parse(argc, argv);
@@ -98,7 +116,7 @@ int main(int argc, char *argv[]) {
   GDALDataset  *poDataset = (GDALDataset *) GDALOpen(command.getInputFilename(), GA_ReadOnly);
   GDALTiler tiler(poDataset);
 
-  writeBounds(tiler);
+  writeBounds(tiler, command.outputDir);
 
   return 0;
 }
