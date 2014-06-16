e5132f889702b118cd63a6a63b93e2e9cc897142
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-06-16 12:19:03 +0100

    Create `terrain.hpp` to access all `libterrain` definitions
    This will be useful for any third party code that links to
    `libterrain`. It is not used internally by the project.

---
+++ src/terrain.hpp
@@ -0,0 +1,11 @@
+/**
+ * terrain.hpp - definitions for working with libterrain
+ */
+
+#include "terrain/types.hpp"
+#include "terrain/TerrainException.hpp"
+#include "terrain/GlobalGeodetic.hpp"
+#include "terrain/TileCoordinate.hpp"
+#include "terrain/TerrainTile.hpp"
+#include "terrain/TileIterator.hpp"
+#include "terrain/GDALTiler.hpp"
