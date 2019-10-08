52d45da346af66a576d869dac71ad8f194b49cee
Alvaro Huarte <ahuarte47@yahoo.es>
2019-10-08 17:30:28 +0200

    Update define GDAL3x in GDALTiler.cpp

--- src/GDALTiler.cpp
+++ src/GDALTiler.cpp
@@ -246,7 +246,7 @@ getOverviewDataset(GDALDatasetH hSrcDS, GDALTransformerFunc pfnTransformer, void
               if( iOvr >= 0 )
                 {
                   //std::cout << "CTB WARPING: Selecting overview level " << iOvr << " for output dataset " << nPixels << "x" << nLines << std::endl;
-                #if ( GDAL_VERSION_MAJOR >= 2 && GDAL_VERSION_MINOR >= 2 )
+                #if ( GDAL_VERSION_MAJOR > 3 || (GDAL_VERSION_MAJOR >= 2 && GDAL_VERSION_MINOR >= 2 ))
                   poSrcOvrDS = GDALCreateOverviewDataset( poSrcDS, iOvr, FALSE );
                 #else
                   poSrcOvrDS = GDALCreateOverviewDataset( poSrcDS, iOvr, FALSE, FALSE );
